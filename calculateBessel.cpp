70c60d75765491b61c95cc7bc42a0c20286dec18
AdwinKing <jin2000112014@gmail.com>
2020-09-04 21:13:20 +0800

    added support for cleaning unnecessary data

--- calculateBessel.cpp
+++ calculateBessel.cpp
@@ -9,25 +9,18 @@
 const double EPSILON = std::pow(2, -52);
 const double FACTOR = std::pow(2, 52);
 
-std::vector<double> besselM(const double x, const unsigned m) {
-    auto ys = std::vector<double>(m + 1);
+
+std::tuple<std::vector<double>, size_t> besselM(const double x, const size_t m) {
+    auto ys = std::vector<double>(m + 1, 0);
     ys[m] = 0;
     ys[m - 1] = 1;
     const double yrcp = 2.0 / x;
-    for (unsigned i = m - 1; i > 0; --i){
-        //results differ from the answers by a factor which is kind of inversely proportional to x; the sum identity implementation may have some problem.
+    for (size_t i = m - 1; i > 0; --i){
         double y = double(i) * ys[i] * yrcp - ys[i + 1];
-        //if y is smaller than the precision of the machine.
-        if (y < EPSILON) {
-            //std::cout << "reached the smallest precision" << std::endl;
-            y *= FACTOR;
-            for (unsigned j = i; j < m; ++j) {
-                ys[j] *= FACTOR;
-            }
-        } else if (y > FACTOR) {
-            //std::cout << "reached the upper bound" << std::endl;
+        //if y is a little too large.
+        if (std::abs(y) > FACTOR) {
             y *= EPSILON;
-            for (unsigned j = i; j < m; ++j) {
+            for (size_t j = i; j < m; ++j) {
                 ys[j] *= EPSILON;
             }
         }
@@ -36,37 +29,48 @@ std::vector<double> besselM(const double x, const unsigned m) {
 
     // Normalize the values by the sum identity.
     double sum = 0;
-    for (auto y : ys) {
+    for (const auto &y : ys) {
         sum += y*y;
     }
-    sum = sum * 2.0 - ys[0] * ys[0]; // forgot to square ys[0]. ಥ_ಥ
+    sum = sum * 2.0 - ys[0] * ys[0];
     const double factor = 1.0 / std::sqrt(sum);
-    for (auto &y : ys) {
-        y *= factor;
+
+    //remove elements smaller than EPSILON.
+    size_t sliceEndIndex = m;
+    for (size_t i = 0; i <= m; ++i) {
+        ys[i] *= factor;
+        if (std::abs(ys[i]) < EPSILON) {
+            sliceEndIndex = i;
+            break;
+        }
     }
-    return ys;
+    //valarray cannot be used with matplotlib so we stick to vector.
+    auto validYS = std::vector<double>(ys.begin(), ys.begin() + long(sliceEndIndex));
+    return std::make_tuple(validYS, sliceEndIndex);
+    //indices are exclusive.
 }
 
 
     
 int main() {
     namespace plt = matplotlibcpp;
-    constexpr unsigned ORDER = 2048;
-    auto xs = std::vector<double>(ORDER + 1);
-    for (unsigned i = 0; i < ORDER; ++i) {
-        xs[i] = double(i);
-    }
-    for (unsigned i = 0; i < 3; ++i) {
-        auto ys = besselM(std::pow(10, i), ORDER);
-        plt::named_plot(std::to_string(std::pow(10, i)), xs, ys);
-        std::cout << "First ten terms of J(" << std::pow(10, i) << ", n)" << std::endl;
+    constexpr size_t ORDER = 512;
+    constexpr size_t EXAMPLE_NUM = 3;
+    for (size_t i = 0; i < EXAMPLE_NUM; ++i) {
+        std::vector<double> ys;
+        size_t endIndex;
+        std::tie(ys, endIndex) = besselM(std::pow(10, i), ORDER);
+        auto xs = std::vector<double>(endIndex);
+        std::iota(xs.begin(), xs.end(), 0);
+        plt::subplot(long(EXAMPLE_NUM), 1, long(i + 1));
+        plt::plot(xs, ys);
+        plt::ylabel(std::to_string(std::pow(10, i)));
+        std::cout << "ten terms of J(" << std::pow(10, i) << ", n)" << std::endl;
         std::cout << "n : J" << std::endl;
-        for (unsigned j = 0; j < 10; ++j) {
+        for (size_t j = 0; j < 10; ++j) {
             std::cout << j << " : " << ys[j] << std::endl;
         }
     }
-    plt::xlim(0, 32);
-    plt::legend();
     plt::show();
     return 0;
 }
